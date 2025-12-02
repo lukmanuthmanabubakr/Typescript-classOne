bbb21bb94f504de4d4bcce36efe8b3f3f04000fc
lukmanuthmanabubakr <lukmanuthman135@gmail.com>
2025-12-02 15:11:35 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const menu = [
+    {name: "Dominus Pizza", price: 500},
+    {name: "Legend Pizza", price: 800},
+    {name: "Safari Pizza", price: 200},
+    {name: "DLT Pizza", price: 100},
+]
+
+const cashInRegister = 100
+const orderQueue = []
