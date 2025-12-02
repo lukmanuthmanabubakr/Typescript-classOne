d9dfde2dfaa58297fe6816db5ae416e08c568439
lukmanuthmanabubakr <lukmanuthman135@gmail.com>
2025-12-02 20:37:36 +0100

    practicing js

--- index.js
+++ index.js
@@ -1,22 +1,21 @@
 const menu = [
-    {name: "Dominus Pizza", price: 500},
-    {name: "Legend Pizza", price: 800},
-    {name: "Safari Pizza", price: 200},
-    {name: "DLT Pizza", price: 100},
-]
+  { name: "Dominus Pizza", price: 500 },
+  { name: "Legend Pizza", price: 800 },
+  { name: "Safari Pizza", price: 200 },
+  { name: "DLT Pizza", price: 100 },
+];
 
-const cashInRegister = 100
-const orderQueue = []
+const cashInRegister = 100;
+const orderQueue = [];
 
 // Add a utility function "addNewPizza" thats take pizza object
 // And add it to the menu
 
 const addNewPizza = (getItem) => {
-    menu.push(getItem)
-}
+  menu.push(getItem);
+};
 
-
-// Write another utility function placeOrder, that takes the pizza name parameter and: 
+// Write another utility function placeOrder, that takes the pizza name parameter and:
 // 1 find thats pizza object in the menu
 // 2. Add the income to the cashInRegister
 // 3. Pushes a new "Order object" to the orderQueue
@@ -24,19 +23,40 @@ const addNewPizza = (getItem) => {
 // return the new order object
 
 const placeOrder = (pizzaItem) => {
-    const findPizza = menu.find(getItem => getItem.name === pizzaItem)
-    cashInRegister += findPizza.price
-    const newOrder = {pizza: findPizza, status: "ordered"}
-    orderQueue.push(newOrder)
-    return newOrder
-}
+  const findPizza = menu.find((getItem) => getItem.name === pizzaItem);
+  cashInRegister += findPizza.price;
+  const newOrder = { pizza: findPizza, status: "ordered" };
+  orderQueue.push(newOrder);
+  return newOrder;
+};
 
 const completeOrder = (orderId) => {
-    const findOrder = orderQueue.find(addNewPizza => getItem.status)
-}
+  const findOrder = orderQueue.find((addNewPizza) => getItem.status);
+};
 
 //Task 1
-let age = 5
-const name = "Abubakr"
+let age = 5;
+const name = "Abubakr";
 
 // Task 2
+const phone = {
+  brand: "Iphone",
+  model: "ios26",
+  price: 500,
+};
+const fruit = ["Orange", "Apple", "Banana"];
+
+// Task 4
+const doubleNumber = () => {
+  let singleNumber;
+  const result = singleNumber * 2;
+  return result;
+};
+
+// Taks 5
+const number = ""
+if (number > 10){
+    return "Big Number"
+}else{
+    return "Small Number"
+}
