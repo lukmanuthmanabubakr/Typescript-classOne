9e78c5e732ab2214484a72548808cd4facb39d9b
lukmanuthmanabubakr <lukmanuthman135@gmail.com>
2025-12-03 21:41:23 +0100

    updatee

--- script.js
+++ script.js
@@ -1,10 +1,39 @@
 // To show Notification we use
-alert("My name is dev Abu")
+alert("My name is dev Abu");
 
 // instead of writing in  my html i can use this
-document.write("My name is dev abu")
-document.write("My name is Abubakr")
-document.write("A developer that know noting")
+document.write("My name is dev abu");
+document.write("My name is Abubakr");
+document.write("A developer that know noting");
 
 console.log("I know i will get better ");
 
+let x = 10;
+
+if (x > 5) {
+  let y = 20;
+  console.log(y);
+}
+
+const street = "Ikeshi";
+
+// Global scope
+
+var xDev = "Hello dev Abu";
+
+function example() {
+  console.log(xDev);
+}
+example();
+console.log(xDev);
+
+//Function scope
+function exampleTwo() {
+  var fs = "Hello Dev dev Abu";
+
+  console.log(fs);
+}
+exampleTwo()
+  console.log(fs);
+
+// block scope
