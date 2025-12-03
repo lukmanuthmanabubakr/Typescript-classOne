3c6c615c8f644c2413e101de7ba6cd2659a1ea2a
lukmanuthmanabubakr <lukmanuthman135@gmail.com>
2025-12-03 18:22:16 +0100

    update

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+// To show Notification we use
 alert("My name is dev Abu")
+
+// instead of writing in  my html i can use this
+document.write("My name is dev abu")
+document.write("My name is Abubakr")
+document.write("A developer that know noting")
+
+console.log("I know i will get better ");
