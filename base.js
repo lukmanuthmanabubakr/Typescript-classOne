db045267f9b958ef54f4766687cb9ab3675a486c
lukmanuthmanabubakr <lukmanuthman135@gmail.com>
2025-12-07 00:08:00 +0100

    update

--- base.js
+++ base.js
@@ -6,10 +6,17 @@
 // console.log(x < 0 && y > 0);
 // console.log(x < 0 && y < 0);
 
-let a = 5;
-let b = 10;
+// let a = 5;
+// let b = 10;
 
-console.log(a > 0 || b > 0);
-console.log(a > 0 || b < 0);
-console.log(a < 0 || b > 0);
-console.log(a < 0 || b < 0);
+// console.log(a > 0 || b > 0);
+// console.log(a > 0 || b < 0);
+// console.log(a < 0 || b > 0);
+// console.log(a < 0 || b < 0);
+
+let Yes = true;
+let No = false;
+
+console.log(!Yes);
+console.log(!No);
+
