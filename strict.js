6268c511b82c43c054687eefebee53180fc57cdf
lukmanuthmanabubakr <lukmanuthman135@gmail.com>
2025-12-13 15:55:47 +0100

    update

---
+++ strict.js
@@ -0,0 +1,13 @@
+function miniCalc(a, b, sign) {
+  let result;
+  if (sign == "+") {
+    result = a + b;
+  }
+}
+miniCalc(3, 6, "+");
+
+function checkAge(age) {
+  let result = age > 18 ? "Adult" : "Minor";
+  console.log(result);
+}
+checkAge(5)
