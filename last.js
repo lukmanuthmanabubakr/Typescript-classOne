4af1b8e3d68de20d2203ce29b52a94a3a60ddb66
lukmanuthmanabubakr <lukmanuthman135@gmail.com>
2025-12-19 22:04:17 +0100

    update

--- last.js
+++ last.js
@@ -1,3 +1,5 @@
+// Task 1
+
 function todaysDate() {
   let date = new Date();
 
@@ -7,5 +9,40 @@ function todaysDate() {
 
   return `${year}-${month}-${numDate}`;
 }
-
 console.log(todaysDate());
+
+// Task 2
+function todaysTime() {
+  let date = new Date();
+
+  let hour = date.getHours();
+  let mins = date.getMinutes();
+
+  return mins < 10 ? `${hour}:0${mins}` : `${hour}:${mins}`;
+}
+console.log(todaysTime());
+
+// Task 3
+function checkPartDay() {
+  let date = new Date();
+
+  let hour = date.getHours();
+  console.log(hour);
+
+  if (hour < 11) {
+    return "Morning";
+  } else if (hour < 16) {
+    return "Afternoon";
+  } else if (hour < 20) {
+    return "Evening";
+  } else if (hour >= 21 && hour >= 4) {
+    return "Night";
+  }
+}
+console.log(checkPartDay());
+
+
+// Task 4
+function timeLeft () {
+    let date = new Date
+}
