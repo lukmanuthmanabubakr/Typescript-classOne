f52549c7b7bd3a2ab063b0166962d25eb00fe2cf
lukmanuthmanabubakr <lukmanuthman135@gmail.com>
2025-12-24 16:32:45 +0100

    update

--- stage2Retake.js
+++ stage2Retake.js
@@ -1,3 +1,8 @@
 let taskForm = document.getElementById("taskForm");
 let taskTitle = document.getElementById("taskTitle");
-let taskForm = document.getElementById("taskForm");
+let taskPriority = document.getElementById("taskPriority");
+let taskStatus = document.getElementById("taskStatus");
+let taskCount = document.getElementById("taskCount");
+let filterTasks = document.getElementById("filterTasks");
+let taskList = document.getElementById("taskList");
+
