4251cf9d46a57026a3a2a965e1b615b6576dbbd2
lukmanuthmanabubakr <lukmanuthman135@gmail.com>
2025-12-24 17:24:08 +0100

    update

--- stage2Retake.js
+++ stage2Retake.js
@@ -6,3 +6,40 @@ let taskCount = document.getElementById("taskCount");
 let filterTasks = document.getElementById("filterTasks");
 let taskList = document.getElementById("taskList");
 
+let theCount = 0;
+let arrOfTask = [];
+
+function filterArrayTask() {
+  let eachStatus;
+  arrOfTask.filter((each) => {
+    each.taskStatus = eachStatus;
+  });
+}
+
+function taskTask(event) {
+  event.preventDefault();
+  let taskInp = taskTitle.value;
+  let taskPrioInp = taskPriority.value;
+  let taskStatusInp = taskStatus.value;
+  let filterTasksInp = filterTasks.value;
+  let taskListDisplay = taskList.value;
+
+  if (!taskInp || taskPrioInp.value == "" || taskStatusInp == "") {
+    return null;
+  }
+
+  arrOfTask.push({ taskInp, taskPrioInp, taskStatusInp });
+  console.log(arrOfTask);
+  theCount += 1;
+  taskCount.textContent = theCount;
+
+  taskList.innerHTML = "";
+
+  arrOfTask.forEach((eachTask) => {
+    let li = document.createElement("li");
+    li.textContent = `Name: ${eachTask.taskInp}, Priority: ${eachTask.taskPrioInp} Status: ${eachTask.taskStatusInp}`;
+    taskList.appendChild(li);
+  });
+}
+
+taskForm.addEventListener("submit", taskTask);
