2dee1bd333176d5e51ff3165e5b301d7088120e0
lukmanuthmanabubakr <lukmanuthman135@gmail.com>
2025-12-30 08:52:01 +0100

    update

---
+++ stage4/layer1.js
@@ -0,0 +1,23 @@
+let loadData = document.getElementById("loadData");
+let loadWithError = document.getElementById("loadWithError");
+let status = document.getElementById("status");
+let dataList = document.getElementById("dataList");
+
+function toLoadData() {
+  status.textContent = "loading...";
+
+  setTimeout(() => {
+    dataList.textContent = "Data Loaded";
+    status.textContent = "Idle";
+  }, 3000);
+}
+loadData.addEventListener("click", toLoadData);
+function toErrorData() {
+  status.textContent = "loading...";
+
+  setTimeout(() => {
+    dataList.textContent = "code error";
+    status.textContent = "Idle";
+  }, 3000);
+}
+loadWithError.addEventListener("click", toErrorData);
